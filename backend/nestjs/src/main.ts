2d3fe9c0cbc06b27442f5e55300226a63e46a443
TrueJarod <erngorn@gmail.com>
2023-07-06 17:09:27 +0300

    add swager

--- backend/nestjs/src/main.ts
+++ backend/nestjs/src/main.ts
@@ -1,18 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
-import { WsAdapter } from '@nestjs/platform-ws';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { UwsAdapter } from './uws/uws.adapter';
 
 async function bootstrap() {
+  // Fastify
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
-  // app.useWebSocketAdapter(new WsAdapter(app));
+  // UWS
   app.useWebSocketAdapter(new UwsAdapter(8080));
   app.setGlobalPrefix('api/v1');
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Playground')
+    .setDescription('The playground API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  // Server
   await app.listen(7001, '0.0.0.0');
 }
 bootstrap();
