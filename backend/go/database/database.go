84c9e7506e4aa8bdd660c554af9470c638c4c60c
TrueJarod <erngorn@gmail.com>
2023-07-15 00:06:03 +0300

    add connection pools

--- backend/go/database/database.go
+++ backend/go/database/database.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -22,12 +23,26 @@ func init() {
 
 func ConnectDb() {
 	connectionString := getConnectionString()
-	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{})
-
+	db, err := gorm.Open(postgres.Open(connectionString), &gorm.Config{
+		PrepareStmt:            true,
+		SkipDefaultTransaction: true,
+	})
 	if err != nil {
 		log.Fatal("Failed to connect to db! \n", err.Error())
 		os.Exit(2)
 	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatal("Failed to retrieve underlying SQL database! \n", err.Error())
+		os.Exit(2)
+	}
+	// Set connection pool configuration
+	maxIdleConnections := 100 // Maximum number of idle connections in the pool
+	maxOpenConnections := 300 // Maximum number of open connections in the pool
+	sqlDB.SetMaxIdleConns(maxIdleConnections)
+	sqlDB.SetMaxOpenConns(maxOpenConnections)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	log.Println("Connected to the db successfully!")
 	db.Logger = logger.Default.LogMode(logger.Info)
